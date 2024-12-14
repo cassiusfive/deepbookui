b28af8464d5f6bb7f5122a7287396d494d545e39
owenkrause <owenjkrause@gmail.com>
2024-12-13 14:22:19 -1000

    feat: deepbook context

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 import { getFullnodeUrl } from "@mysten/sui/client";
+import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { initializeDeepBook } from '@/lib/deepbook/client';
+import { DeepBookProvider } from '@/contexts/deepbook';
 
 import "@mysten/dapp-kit/dist/index.css";
 import "./index.css";
@@ -14,13 +17,18 @@ const networks = {
   mainnet: { url: getFullnodeUrl("mainnet") },
 };
 
+const keypair_ed25519 = new Ed25519Keypair();
+const dbClient = initializeDeepBook(keypair_ed25519.getSecretKey(), "mainnet");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <SuiClientProvider networks={networks} defaultNetwork="devnet">
-        <WalletProvider>
-          <App />
-        </WalletProvider>
+      <SuiClientProvider networks={networks} defaultNetwork="mainnet">
+        <DeepBookProvider client={dbClient}>
+          <WalletProvider>
+            <App />
+          </WalletProvider>
+        </DeepBookProvider>
       </SuiClientProvider>
     </QueryClientProvider>
   </StrictMode>,
