122dd88938e1246a017c96dd67f6fe2cd700b7b7
owenkrause <owenjkrause@gmail.com>
2025-01-30 03:19:15 -0800

    feat: switch between network

---
+++ src/contexts/network.tsx
@@ -0,0 +1,51 @@
+import { createContext, useState, useContext } from "react";
+
+type Network = "mainnet" | "testnet";
+
+type NetworkProviderProps = {
+  children: React.ReactNode;
+  defaultNetwork?: Network;
+  storageKey?: string;
+};
+
+type NetworkProviderState = {
+  network: Network;
+  setNetwork: (network: Network) => void;
+  storageKey?: string;
+};
+
+const initialState: NetworkProviderState = {
+  network: "mainnet",
+  setNetwork: () => null,
+};
+
+const NetworkProviderContext = createContext<NetworkProviderState>(initialState);
+
+export function NetworkProvider({
+  children,
+  defaultNetwork = "mainnet",
+  storageKey = "network",
+  ...props
+}: NetworkProviderProps) {
+  const [network, setNetwork] = useState<Network>(
+    () => (localStorage.getItem(storageKey) as Network) || defaultNetwork,
+  );
+
+  const value = {
+    network,
+    setNetwork: (network: Network) => {
+      localStorage.setItem(storageKey, network);
+      setNetwork(network);
+    }
+  }
+  
+  return (
+    <NetworkProviderContext.Provider {...props} value={value}>
+      {children}
+    </NetworkProviderContext.Provider>
+  );
+}
+
+export const useNetwork = () => {
+  return useContext(NetworkProviderContext);
+};
