962b324beaf681622f079d017bab8a830534c4a5
owenkrause <owenjkrause@gmail.com>
2025-01-30 23:35:32 -0800

    feat: added trade history and settled balances tab to open orders

--- src/components/user-orders.tsx
+++ src/components/user-orders.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
 import { Transaction } from "@mysten/sui/transactions";
 import { useCurrentPool } from "@/contexts/pool";
 import { useDeepBook } from "@/contexts/deepbook";
 import { useOpenOrders } from "@/hooks/useOpenOrders";
 import { BALANCE_MANAGER_KEY } from "@/constants/deepbook";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 import {
   Table,
   TableBody,
@@ -12,8 +19,9 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
-import { BookX } from "lucide-react";
+import { BookX, Loader2 } from "lucide-react";
 import { useSignAndExecuteTransaction } from "@mysten/dapp-kit";
+import { useBalanceManagerAccount } from "@/hooks/useBalanceManagerAccount";
 
 function formatTime(date: Date): string {
   const hours = date.getHours().toString().padStart(2, "0");
@@ -26,14 +34,17 @@ function formatTime(date: Date): string {
 export default function OpenOrders() {
   const dbClient = useDeepBook();
   const pool = useCurrentPool()
-  const { data: openOrders, isLoading } = useOpenOrders(pool.pool_name, BALANCE_MANAGER_KEY)
+  const { data: openOrders } = useOpenOrders(pool.pool_name, BALANCE_MANAGER_KEY)
+  const { data: balanceManagerAccount } = useBalanceManagerAccount(pool.pool_name, BALANCE_MANAGER_KEY)
   const { mutate: signAndExecuteTransaction } = useSignAndExecuteTransaction();
+  const [loadingCancelOrders, setloadingCancelOrders] = useState<Set<string>>(new Set())
+  const [loadingClaimSettledBalances, setLoadingClaimSettledBalances] = useState<Set<string>>(new Set())
 
   if (!dbClient) return
-  if (isLoading) return <div>loading</div>
-  if (!openOrders) return
 
-  const handleCancelOrder = async(orderId: string) => {
+  const handleCancelOrder = (orderId: string) => {
+    setloadingCancelOrders(prev => new Set([...prev, orderId]));
+
     const tx = new Transaction();
     
     dbClient.deepBook.cancelOrder(
@@ -46,55 +57,147 @@ export default function OpenOrders() {
       transaction: tx
     }, {
       onSuccess: result => {
-        console.log('executed transaction', result);
+        console.log("cancled order", result);
       }
     })
+
+    setloadingCancelOrders(prev => {
+      const next = new Set(prev);
+      next.delete(orderId);
+      return next;
+    });
+  }
+
+  const handleClaimSettledFunds = (poolKey: string) => {
+    setLoadingClaimSettledBalances(prev => new Set([...prev, poolKey]));
+
+    const tx = new Transaction()
+
+    dbClient.deepBook.withdrawSettledAmounts(pool.pool_name, BALANCE_MANAGER_KEY)(tx)
+
+    signAndExecuteTransaction({
+      transaction: tx
+    }, {
+      onSuccess: result => {
+        console.log("withdrew settled funds", result);
+      }
+    })
+
+    setLoadingClaimSettledBalances(prev => {
+      const next = new Set(prev);
+      next.delete(poolKey);
+      return next;
+    });
   }
 
   return (
     <div className="h-full">
-      <div className="border-b p-4">Orders</div>
-      <div className="no-scrollbar h-[180px] overflow-y-auto">
-        <Table>
-          <TableHeader className="sticky top-0 text-nowrap bg-background text-xs shadow-[0_0_0_1px_hsl(var(--border))] [&_tr]:border-none">
-            <TableRow>
-              <TableHead className="w-[100px] pl-4">TIME PLACED</TableHead>
-              <TableHead>PAIR</TableHead>
-              <TableHead>TYPE</TableHead>
-              <TableHead>SIDE</TableHead>
-              <TableHead>PRICE</TableHead>
-              <TableHead>AMOUNT</TableHead>
-              <TableHead>TOTAL</TableHead>
-              <TableHead className="pr-4 text-right">STATUS</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody className="text-nowrap text-xs [&_tr]:border-none [&_tr_td:first-child]:pl-4 [&_tr_td:first-child]:text-muted-foreground [&_tr_td:last-child]:pr-4 [&_tr_td:last-child]:text-right">
-          {openOrders.length > 0 &&
-            openOrders.map((order) => {
-              console.log(order)
-              return (
+      <Tabs defaultValue="open-orders">
+        <TabsList className="w-full justify-start rounded-none bg-background px-4 pt-8 pb-6">
+          <TabsTrigger className="data-[state=active]:shadow-none" value="open-orders">Open Orders{openOrders ? ` (${openOrders.length})` : ""}</TabsTrigger>
+          <TabsTrigger className="data-[state=active]:shadow-none" value="trade-history">Trade History</TabsTrigger>
+          <TabsTrigger className="data-[state=active]:shadow-none" value="settled-balance">Settled Balance</TabsTrigger>
+        </TabsList>
+        <div className="mx-4 border-b"></div>
+        <TabsContent value="open-orders" className="mt-0">
+          <div className="relative no-scrollbar h-[180px] overflow-y-auto">
+            <Table>
+              <TableHeader className="sticky top-0 text-nowrap bg-background text-xs [&_tr]:border-none">
                 <TableRow>
-                  <TableCell>{formatTime(new Date(order.expire_timestamp))}</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell>USDC</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell>USDC</TableCell>
-                  <TableCell>{order.status}</TableCell>
-                  <TableCell><Button onClick={() => handleCancelOrder(order.order_id)}>Cancel</Button></TableCell>
+                  <TableHead className="pl-4 text-left">TIME PLACED</TableHead>
+                  <TableHead>PAIR</TableHead>
+                  <TableHead>TYPE</TableHead>
+                  <TableHead>SIDE</TableHead>
+                  <TableHead>PRICE</TableHead>
+                  <TableHead>AMOUNT</TableHead>
+                  <TableHead>TOTAL</TableHead>
+                  <TableHead className="pr-4 text-right">STATUS</TableHead>
                 </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table>
-        {!openOrders.length && (
-          <div className="flex h-[calc(100%-40px)] flex-col items-center justify-center text-xs text-muted-foreground">
-            <BookX />
-            <div>No orders</div>
+              </TableHeader>
+              <TableBody className="text-nowrap text-xs [&_tr]:border-none [&_tr_td:first-child]:pl-4 [&_tr_td:first-child]:text-muted-foreground [&_tr_td:last-child]:pr-4 [&_tr_td:last-child]:text-right">
+                {!openOrders || openOrders?.length === 0 ? (
+                  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-2 items-center justify-center text-xs text-muted-foreground">
+                    <BookX />
+                    <div>No orders</div>
+                  </div>
+                ): openOrders.map((order) => {
+                  console.log(order)
+                  return (
+                    <TableRow>
+                      <TableCell>{formatTime(new Date(order.expire_timestamp))}</TableCell>
+                      <TableCell></TableCell>
+                      <TableCell></TableCell>
+                      <TableCell></TableCell>
+                      <TableCell>USDC</TableCell>
+                      <TableCell></TableCell>
+                      <TableCell>USDC</TableCell>
+                      <TableCell>{order.status}</TableCell>
+                      <TableCell>
+                        <Button variant="outline" className="text-xs" disabled={loadingCancelOrders.has(order.order_id)} onClick={() => handleCancelOrder(order.order_id)}>
+                          { loadingCancelOrders.has(order.order_id) ? 
+                            (
+                              <>
+                                <Loader2 className="animate-spin" />
+                                Please wait
+                              </>
+                            ): <span className="px-4">Cancel</span>
+                          }
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
           </div>
-        )}
-      </div>
+        </TabsContent>
+        <TabsContent value="trade-history" className="mt-0">
+          <Table>
+            <TableHeader className="sticky top-0 text-nowrap bg-background text-xs [&_tr]:border-none">
+                <TableRow>
+                  <TableHead className="pl-4 text-left">TIME PLACED</TableHead>
+                  <TableHead>PAIR</TableHead>
+                  <TableHead>TYPE</TableHead>
+                  <TableHead>SIDE</TableHead>
+                  <TableHead>PRICE</TableHead>
+                  <TableHead>AMOUNT</TableHead>
+                  <TableHead className="pr-4 text-right">TOTAL</TableHead>
+                </TableRow>
+            </TableHeader>
+          </Table>
+        </TabsContent>
+        <TabsContent value="settled-balance" className="mt-0">
+          <Table>
+              <TableHeader className="sticky top-0 text-nowrap bg-background text-xs [&_tr]:border-none">
+                <TableRow>
+                  <TableHead className="pl-4 text-left">PAIR</TableHead>
+                  <TableHead>BASE ASSET AMOUNT</TableHead>
+                  <TableHead>QUOTE ASSET AMOUNT</TableHead>
+                  <TableHead className="pr-4 text-right">ACTION</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody className="text-nowrap text-xs [&_tr]:border-none [&_tr_td:first-child]:pl-4 [&_tr_td:first-child]:text-muted-foreground [&_tr_td:last-child]:pr-4 [&_tr_td:last-child]:text-right">
+                <TableRow>
+                  <TableCell>{pool.pool_name}</TableCell>
+                  <TableCell>{balanceManagerAccount?.settled_balances.base}</TableCell>
+                  <TableCell>{balanceManagerAccount?.settled_balances.quote}</TableCell>
+                  <TableCell>
+                        <Button variant="outline" className="text-xs" disabled={loadingClaimSettledBalances.has(pool.pool_name)} onClick={() => handleClaimSettledFunds(pool.pool_name)}>
+                          { loadingClaimSettledBalances.has(pool.pool_name) ? 
+                            (
+                              <>
+                                <Loader2 className="animate-spin" />
+                                Please wait
+                              </>
+                            ): <span className="px-4">Claim</span>
+                          }
+                        </Button>
+                      </TableCell>
+                </TableRow>
+              </TableBody>
+          </Table>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
