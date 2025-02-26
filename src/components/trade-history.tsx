b7fb08dbdcac6c5ac9ea64710e9dc00a110da731
owenkrause <owenjkrause@gmail.com>
2025-02-26 15:51:49 -0800

    fixed loading state

--- src/components/trade-history.tsx
+++ src/components/trade-history.tsx
@@ -5,8 +5,6 @@ export default function TradeHistory() {
   const pool = useCurrentPool();
   const { data: trades } = useTradeHistory(pool.pool_name);
 
-  if (!trades) return <div>failed to fetch trade history</div>
-
   return (
     <table className="w-full text-xs">
       <thead className="sticky top-0 h-6 bg-background text-right text-muted-foreground shadow-[0_0_0_1px_hsl(var(--border))]">
@@ -17,19 +15,19 @@ export default function TradeHistory() {
         </tr>
       </thead>
       <tbody>
-        {trades.map((trade, index) => (
+        {trades && (trades.map((trade, index) => (
           <tr key={index} className="text-right">
-            <th className="text-nowrap pr-4">{trade.base_volume}</th>
-            <th
+            <td className="text-nowrap pr-4">{trade.base_volume}</td>
+            <td
               className={`text-nowrap pr-6 ${trade.type == "buy" ? "text-[#26a69a]" : "text-[#ef5350]"}`}
             >
               {trade.quote_volume}
-            </th>
-            <th className="text-nowrap pr-3 text-muted-foreground">
+            </td>
+            <td className="text-nowrap pr-3 text-muted-foreground">
               {new Date(trade.timestamp).toLocaleTimeString([], { hour12: false })}
-            </th>
+            </td>
           </tr>
-        ))}
+        )))}
       </tbody>
     </table>
   );
