143fd48c64b088fc32b90ef1ccc1627ab8033f73
owenkrause <owenjkrause@gmail.com>
2025-02-26 19:06:34 -0800

    replaced geckoterminal candlestick data with cetus

--- src/hooks/useCandleData.ts
+++ src/hooks/useCandleData.ts
@@ -1,76 +1,57 @@
 import { useCurrentPool } from "@/contexts/pool";
 import { useQuery } from "@tanstack/react-query";
-import { DEEPBOOK_POOL_TO_DEX_POOL } from "@/constants/geckoterminal";
 
-
-type OHLCVDataPoint = [
-  number,  // time
-  number,  // open
-  number,  // high
-  number,  // low
-  number,  // close
-  number   // volume
-];
-
-type CoinMeta = {
-  address: string;
-  name: string;
-  symbol: string;
-  coingecko_coin_id: string;
-}
-
-type ResponseMeta = {
-  base: CoinMeta;
-  quote: CoinMeta;
-}
-
-type OHLCVAttributes = {
-  ohlcv_list: OHLCVDataPoint[];
+type PriceData = {
+  open: string;
+  high: string;
+  low: string;
+  settle: string;
+  avg: string;
+  timestamp: number;
 }
 
 type ResponseData = {
-  id: string;
-  type: string;
-  attributes: OHLCVAttributes;
+  code: number;
+  msg: string;
+  data: {
+    lists: PriceData[]
+  };
 }
 
-type OHLCVResponse = {
-  data: ResponseData;
-  meta: ResponseMeta;
-}
+async function fetchGeckoTerminalOHLCV(poolId: string) {
+  const end = Math.floor(Date.now() / 1000)
+  const start = end - (60 * 60 * 24 * 7)
 
-async function fetchGeckoTerminalOHLCV(
-  poolId: string,
-  timeframe: "day" | "hour" | "minute",
-) {
-  const dexPool = DEEPBOOK_POOL_TO_DEX_POOL[poolId];
+  const url = `https://api-sui.cetus.zone/v2/sui/deepbookv3/prices?date_type=hour&start_timestamp=${start}&end_timestamp=${end}&address=${poolId}`;
 
-  const url = `https://api.geckoterminal.com/api/v2/networks/sui-network/pools/${dexPool}/ohlcv/${timeframe}`;
+  console.log(url)
 
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`Error fetching data: ${response.status} ${response.statusText}`);
   }
 
-  const data = (await response.json()) as OHLCVResponse;
-  return data.data.attributes.ohlcv_list
-    .map(dataPoint => ({
-      time: dataPoint[0],
-      open: dataPoint[1],
-      high: dataPoint[2],
-      low: dataPoint[3],
-      close: dataPoint[4],
-    }))
-    .sort((a, b) => a.time - b.time);
+  const res = (await response.json()) as ResponseData;
+
+  console.log(res)
+  return res.data.lists.map(dataPoint => ({
+    time: dataPoint.timestamp,
+    open: parseFloat(dataPoint.open),
+    high: parseFloat(dataPoint.high),
+    low: parseFloat(dataPoint.low),
+    close: parseFloat(dataPoint.settle),
+  })).sort((a, b) => a.time - b.time);
 }
 
 export function useCandleData() {
   const pool = useCurrentPool();
 
+  console.log(pool.pool_id)
+
   return useQuery({
     queryKey: ["candleData", pool.pool_id],
     queryFn: async () => {
-      return fetchGeckoTerminalOHLCV(pool.pool_id, "day");
+      return fetchGeckoTerminalOHLCV(pool.pool_id);
     },
     refetchInterval: 10000,
   });
